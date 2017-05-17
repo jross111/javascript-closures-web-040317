e093a225acebf4421df5dce90d103ab639497724
github username <github email address>
2017-05-17 14:04:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const app = "I don't do much."
+
+function fatBastard(meal){
+  function whatsForDinner(){
+    if (meal === 'mini-me'){
+      console.log('The wee man is in my Belly!')
+    } else {
+      console.log(`I'm eatin a bit of ${meal}! Burp.`)
+    }
+  }
+  return whatsForDinner
+
+}
+
+const whatsForDinner = fatBastard('kobe beef');
+
+
+function raspyDoorGuy() {
+  const password = 'yarr'; // password is a local variable created by raspyDoorGuy()
+
+  function givePassword(givenPassword) { // givePassword() is the inner function, a closure
+    if (givenPassword === password) { // givePassword() uses variable declared in the parent function
+      console.log('Ye may enter.');
+    } else {
+      console.log('Begone, landlubber!');
+    }
+  }
+
+  return {
+    givePassword
+  };
+}
